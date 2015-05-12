bf618f004023beda093551b4e07a2ecec98b7b88
Husam Machlovi <hmachlovi@gmail.com>
2015-05-12 13:12:25 -0400

    fixtures update

--- server/fixtures/cases.js
+++ server/fixtures/cases.js
@@ -3,6 +3,83 @@ Fixtures = typeof Fixtures !== "undefined" ? Fixtures : {};
 
 Fixtures.cases = [
   {
+    client: 'With Pulp',
+    effect: 'effect-push',
+    jumbo: '/images/cases/rubyblanc/intro.png',
+    logo: '/images/cases/rubyblanc/ruby.png',
+    title: 'Bringing a Powerhouse Since 1978 Online',
+    sections: [
+      {
+        header: 'With Pulp',
+        paragraphs: [
+          {
+            text: 'With Pulp is my own company, a software development shop. For our branding and web presence, we wanted our personalities and competence to shine.'
+          },
+          {
+            text: 'The competition is still going fierce, both of us with 45 days at the time of writing this. This competition really brought to light the importance of daily shipments, even if they\'re small. After my 30 day streak, I found myself determined to ship every single day.',
+          },
+          {
+            text: 'As a writer, I knew that if I could apply this same principle, my output would be exponentially better. This is where the idea of Writerly surfaced'
+          }
+        ]
+      },
+      {
+        header: 'The Process',
+        paragraphs: [
+          {
+            text: 'I interviewed everyone on the team to understand the personality traits they valued the most, and their favorite hobbies. This served as the data that would drive the design decisions including branding, tone, messaging and art.'
+          },
+          {
+            text: 'I made sense of the data by finding the lowest common denominators',
+            images: [
+              {
+                path: '/images/cases/rubyblanc/colors.png'
+              }
+            ]
+          },
+          {
+            text: ''
+          },
+          {
+            text: '',
+            images: [
+              {
+                path: '/images/cases/rubyblanc/logo.png'
+              }
+            ]
+          }
+        ]
+      },
+      {
+        header: 'The Design',
+        paragraphs: [
+          {
+            text: 'I proposed we push the storytelling idea by choosing our own archetypes and illustrating them. This creative freedom got the team really excited and invested in the process!'
+          },
+          {
+            text: 'I layered the final illustrations over space backgrounds. This mirror some of our favorite video games growing up (Marvel vs Street Fighter and Marvel vs Capcom) and allowed us to put our love for Sci-Fi',
+            images: [
+              {
+                path: '/images/cases/rubyblanc/colors.png'
+              }
+            ]
+          },
+          {
+            text: 'I wanted the messaging to capture our personality and competency. I took an authentic tone and injected fun tidbits where we had the leighway.'
+          },
+          {
+            text: 'I wanted the tagline to embody the collobartive nature of software projects. We\'re very proud of the work we do, and we like to be invested throughout the entire process from planning to execution. With this in mind, I arrived at our current tagline:',
+            images: [
+              {
+                path: '/images/cases/rubyblanc/logo.png'
+              }
+            ]
+          }
+        ]
+      },
+    ]
+  },
+  {
     client: 'Writerly IO',
     effect: 'effect-push',
     jumbo: '/images/cases/rubyblanc/intro.png',
