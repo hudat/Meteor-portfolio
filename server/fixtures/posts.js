46a2a97075913d555f03e8319e0df9aa42d48c36
Husam Machlovi <hmachlovi@gmail.com>
2015-05-21 22:36:19 -0400

    added full posts data, and added url field

--- server/fixtures/posts.js
+++ server/fixtures/posts.js
@@ -4,6 +4,16 @@ Fixtures = typeof Fixtures !== "undefined" ? Fixtures : {};
 Fixtures.posts = [
   {
     title: 'Rapid Growth',
-    excerpt: 'Out of clutter, find simplicity'
+    excerpt: 'For every single observation there are an infinite number of explanations. Through rapid testing, we can discover the minimum effective dose for continuous growth.',
+    url: 'http://blog.husammachlovi.com/rapid-growth/'
+  },
+  {
+    title: '8 Things I\'ve Learned as a Freelancer',
+    excerpt: 'As a freelancer for 4 years, I made a lot of mistakes. I’ve documented my learnings so that I can avoid these pitfalls and ultimately, better manage stress.',
+    url: 'http://blog.husammachlovi.com/8-things-i-learned-as-a-freelance-ux-designer/'
+  },
+    title: 'Increase User Engagement with Rewarding Interactions',
+    excerpt: 'An examination of MailChimp, Photojojo, and GrubHub\'s use of rewarding interactions to build emotional connections with users.',
+    url: 'http://blog.husammachlovi.com/increase-user-engagement-by-rewarding-interactions/'
   }
 ];
