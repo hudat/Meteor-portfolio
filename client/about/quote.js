f373aa00984d5cd6aeb6aa50b5c5086fe3be30a4
Husam Machlovi <hmachlovi@gmail.com>
2015-05-21 22:55:50 -0400

    refactored quote template

--- client/about/quote.js
+++ client/about/quote.js
@@ -1,1 +1,1 @@
-Template.quote.helpers({  randomQuote: function() {    var quotes = Quotes.find().fetch();    var quote = quotes[Math.floor(Math.random()*quotes.length)];    return [quote];  },  quotes: function() {    return Quotes.find();  }});
+Template.quote.helpers({  quotes: function() {    var quotes = Quotes.find().fetch();    var quote = quotes[Math.floor(Math.random()*quotes.length)];    return [quote];    return Quotes.find();  }});
