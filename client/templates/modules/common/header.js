62c58b7be3b465859db8c0f68be2041d84758df1
Husam Machlovi <husam@withpulp.com>
2015-09-19 15:34:21 -0400

    sidebar operational

--- client/templates/modules/common/header.js
+++ client/templates/modules/common/header.js
@@ -2,5 +2,9 @@ Template.header.events({
   'click .menu-button': function() {
     var mobileMenu = document.getElementById('#mobile-menu');
     mobileMenu.scrollIntoView();
-  }
+  },
+  'click .logo': function() {
+      //Session.set('menuToggle', !Session.get('menuToggle'));
+      $('.ui.sidebar').sidebar('toggle');
+      }
 });
--- client/templates/modules/common/sidebar.js
+++
@@ -1,6 +0,0 @@
-Template.sidebar.events({
-    'click .logo': function() {
-        //Session.set('menuToggle', !Session.get('menuToggle'));
-        $('.ui.sidebar').sidebar('toggle');
-    }
-});
