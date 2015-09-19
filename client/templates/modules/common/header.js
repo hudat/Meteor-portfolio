7c848ac2cb3eaf940d58f076d6859483abe6aacf
Husam Machlovi <husam@withpulp.com>
2015-09-19 15:46:55 -0400

    mobile menu functional

--- client/templates/modules/common/header.js
+++ client/templates/modules/common/header.js
@@ -1,8 +1,10 @@
 Template.header.events({
-  'click .menu-button': function() {
-    var mobileMenu = document.getElementById('#mobile-menu');
-    mobileMenu.scrollIntoView();
-  },
+    'click #menu-button':function(e,tmpl) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("#mobile-menu").offset().top
+        }, 600);
+     },
   'click .logo': function() {
       //Session.set('menuToggle', !Session.get('menuToggle'));
       $('.ui.sidebar').sidebar('toggle');
