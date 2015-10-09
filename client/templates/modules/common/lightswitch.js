706c28616632a82ffbf921639f81d58110a5727d
Husam Machlovi <husam@withpulp.com>
2015-10-09 07:54:59 -0400

    switch animation

--- client/templates/modules/common/lightswitch.js
+++ client/templates/modules/common/lightswitch.js
@@ -1,14 +1,22 @@
 Template.lightswitch.events({
     'click #light':function() {
-        $("body").removeClass("light-theme");
-        $("body").addClass("dark-theme");
+        $("body").fadeOut(30, function() {
+              $("body").removeClass("light-theme");
+        });
+        $("body").fadeIn(0, function() {
+          $("body").addClass("dark-theme");
+        });
         $("#light").addClass("hide");
         $("#dark").removeClass("hide");
       },
       'click #dark':function() {
+        $("body").fadeOut(30, function() {
           $("body").removeClass("dark-theme");
+        });
+        $("body").fadeIn(0, function() {
           $("body").addClass("light-theme");
-          $("#dark").addClass("hide");
-          $("#light").removeClass("hide");
+        });
+        $("#dark").addClass("hide");
+        $("#light").removeClass("hide");
         }
 });
