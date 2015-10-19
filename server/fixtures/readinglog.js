62affb9ced40a90e1a5f25fb931e4150bfa119c6
Husam Machlovi <husam@withpulp.com>
2015-10-19 11:43:55 -0400

    reading log wiring done

--- server/fixtures/readinglog.js
+++ server/fixtures/readinglog.js
@@ -6,19 +6,79 @@ Fixtures.readinglog = [
     title: 'Best Design Books',
     books: [
       {
-       Booktitle: 'Mobile-First Design'
+       booktitle: 'Mobile-First Design'
       },
       {
-       Booktitle: 'Designing With Emotion'
+       booktitle: 'Designing With Emotion'
       },
       {
-       Booktitle: 'The Design of Everyday Things'
+       booktitle: 'The Design of Everyday Things'
       },
       {
-       Booktitle: 'Don\'t Make Me Think'
+       booktitle: 'Don\'t Make Me Think'
       },
       {
-       Booktitle: 'Emotional Design'
+       booktitle: 'Emotional Design'
+      }
+    ]
+  },
+  {
+    title: 'Best Non-Design Books For Design',
+    books: [
+      {
+       booktitle: 'The Writer\'s Journey'
+      },
+      {
+       booktitle: 'Drive'
+      },
+      {
+       booktitle: 'The 22 Immutable Laws of Marketing'
+      },
+      {
+       booktitle: 'The War Of Art'
+      },
+      {
+       booktitle: 'Art Of Learning'
+      }
+    ]
+  },
+  {
+    title: 'Fiction',
+    books: [
+      {
+       booktitle: '1984'
+      },
+      {
+       booktitle: 'Dune'
+      },
+      {
+       booktitle: 'Crime & Punishment'
+      },
+      {
+       booktitle: 'Shantaram'
+      },
+      {
+       booktitle: 'The Diamond Age'
+      }
+    ]
+  },
+  {
+    title: 'Non-Fiction',
+    books: [
+      {
+       booktitle: 'The 4-Hour Workweek'
+      },
+      {
+       booktitle: 'Metaphors We Live By'
+      },
+      {
+       booktitle: 'A Whole New Mind'
+      },
+      {
+       booktitle: 'Think And Grow Rich'
+      },
+      {
+       booktitle: 'As A Man Thinketh'
       }
     ]
   }
