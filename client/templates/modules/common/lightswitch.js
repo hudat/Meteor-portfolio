3c76809f97735b6ba4216e384644b41a18789f5d
Husam Machlovi <husam@withpulp.com>
2015-12-12 17:29:18 -0500

    reverse theme toggle method functional

--- client/templates/modules/common/lightswitch.js
+++ client/templates/modules/common/lightswitch.js
@@ -1,39 +1,58 @@
 Template.lightswitch.events({
+
     'click #light':function() {
 
-      var bounce = function() {
-        for (var i=1; i<=3; i++) {
-          $(this).animate({top: 30},"fast");
-          $(this).animate({top: 0},"fast");
-        };
-      }
+        var bounce = function() {
+          for (var i=1; i<=3; i++) {
+            $(this).animate({top: 40},"fast");
+            $(this).animate({top: 0},"fast");
+          };
+        }
 
-      var themeToggle = function() {
-        $("body").fadeOut(30, function() {
-         $("body").removeClass("light-theme");
-         });
-         $("body").fadeIn(0, function() {
-           $("body").addClass("dark-theme");
-         });
-         $("#light").addClass("hide");
-         $("#dark").removeClass("hide");
-       }
+        var themeToggle = function() {
+          $("body").fadeOut(30, function() {
+           $("body").removeClass("light-theme");
+           });
+           $("body").fadeIn(0, function() {
+             $("body").addClass("dark-theme");
+           });
+           $("#light").addClass("hide");
+           $("#dark").removeClass("hide");
+         }
 
-       bounce();
+         bounce();
 
-       setTimeout(function() {
-         themeToggle();
-       }, 250);
+         setTimeout(function() {
+           themeToggle();
+         }, 250);
 
       },
+
       'click #dark':function() {
-        $("body").fadeOut(30, function() {
-          $("body").removeClass("dark-theme");
-        });
-        $("body").fadeIn(0, function() {
-          $("body").addClass("light-theme");
-        });
-        $("#dark").addClass("hide");
-        $("#light").removeClass("hide");
+
+          var bounce = function() {
+            for (var i=1; i<=3; i++) {
+              $(this).animate({top: 40},"fast");
+              $(this).animate({top: 0},"fast");
+            };
+          }
+
+          var themeToggle = function() {
+            $("body").fadeOut(30, function() {
+              $("body").removeClass("dark-theme");
+            });
+            $("body").fadeIn(0, function() {
+              $("body").addClass("light-theme");
+            });
+            $("#dark").addClass("hide");
+            $("#light").removeClass("hide");
+          }
+
+          bounce();
+
+          setTimeout(function() {
+            themeToggle();
+          }, 250);
+
         }
   });
