a1b83deccaa33c5f424680da1433567911ef0f36
Husam Machlovi <husam@withpulp.com>
2015-12-13 17:41:56 -0500

    lightswitch updates

--- client/templates/modules/common/lightswitch.js
+++ client/templates/modules/common/lightswitch.js
@@ -3,28 +3,28 @@ Template.lightswitch.events({
     'click #light':function() {
 
         var bounce = function() {
-          for (var i=1; i<=3; i++) {
-            $(this).animate({top: 80},"fast");
-            $(this).animate({top: 0},"fast");
-          };
+            $(this).animate({top: -40},"slow");
+            $(this).animate({top: -0},"slow");
+            $(this).animate({top: -70},"slow");
         }
 
-        var themeToggle = function() {
-          $("body").fadeOut(30, function() {
-           $("body").removeClass("light-theme");
-           });
-           $("body").fadeIn(0, function() {
-             $("body").addClass("dark-theme");
-           });
-           $("#light").addClass("hide");
-           $("#dark").removeClass("hide");
-         }
-
-         bounce();
-
-         setTimeout(function() {
-           themeToggle();
-         }, 250);
+        // var themeToggle = function() {
+        //   $("body").fadeOut(30, function() {
+        //    $("body").removeClass("light-theme");
+        //    });
+        //    $("body").fadeIn(0, function() {
+        //      $("body").addClass("dark-theme");
+        //    });
+        //    $("#light").addClass("hide");
+        //    $("#dark").removeClass("hide");
+        //  }
+        //
+        //
+        //
+        //  setTimeout(function() {
+        //    themeToggle();
+        //  }, 1000);
+        bounce();
 
       },
 
@@ -32,7 +32,7 @@ Template.lightswitch.events({
 
           var bounce = function() {
             for (var i=1; i<=3; i++) {
-              $(this).animate({top: 80},"fast");
+              $(this).animate({top: 40},"fast");
               $(this).animate({top: 0},"fast");
             };
           }
