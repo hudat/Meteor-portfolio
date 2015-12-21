539ff1da949199825e4176d3391a8d6ac9324c15
Husam Machlovi <husam@withpulp.com>
2015-12-21 15:30:57 -0500

    disabled lightswitch, tagline type update

--- client/templates/modules/common/lightswitch.js
+++ client/templates/modules/common/lightswitch.js
@@ -1,41 +1,41 @@
-Template.lightswitch.events({
-
-    'click #light':function() {
-
-        var themeToggle = function() {
-          $("body").fadeOut(30, function() {
-           $("body").removeClass("light-theme");
-           });
-           $("body").fadeIn(0, function() {
-             $("body").addClass("dark-theme");
-           });
-           $("#light").addClass("hide");
-           $("#dark").removeClass("hide");
-         }
-
-         setTimeout(function() {
-           themeToggle();
-         }, 250);
-
-      },
-
-      'click #dark':function() {
-
-          var themeToggle = function() {
-            $("body").fadeOut(30, function() {
-              $("body").removeClass("dark-theme");
-            });
-            $("body").fadeIn(0, function() {
-              $("body").addClass("light-theme");
-            });
-            $("#dark").addClass("hide");
-            $("#light").removeClass("hide");
-          }
-
-
-          setTimeout(function() {
-            themeToggle();
-          }, 250);
-
-        }
-  });
+// Template.lightswitch.events({
+//
+//   'click #light': function() {
+//
+//     var themeToggle = function() {
+//       $('body').fadeOut(30, function() {
+//         $('body').removeClass('light-theme');
+//       });
+//       $('body').fadeIn(0, function() {
+//         $('body').addClass('dark-theme');
+//       });
+//       $('#light').addClass('hide');
+//       $('#dark').removeClass('hide');
+//     }
+//
+//     setTimeout(function() {
+//       themeToggle();
+//     }, 250);
+//
+//   },
+//
+//   'click #dark': function() {
+//
+//     var themeToggle = function() {
+//       $('body').fadeOut(30, function() {
+//         $('body').removeClass('dark-theme');
+//       });
+//       $('body').fadeIn(0, function() {
+//         $('body').addClass('light-theme');
+//       });
+//       $('#dark').addClass('hide');
+//       $('#light').removeClass('hide');
+//     }
+//
+//
+//     setTimeout(function() {
+//       themeToggle();
+//     }, 250);
+//
+//   }
+// });
