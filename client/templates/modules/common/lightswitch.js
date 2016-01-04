76926d3a66bc9d48ef31ea25d497b4fcf9729d5c
Husam Machlovi <husam@withpulp.com>
2016-01-03 22:15:27 -0500

    lightswitch work, stylistic update

--- client/templates/modules/common/lightswitch.js
+++ client/templates/modules/common/lightswitch.js
@@ -1,41 +1,42 @@
-// Template.lightswitch.events({
-//
-//   'click #light': function() {
-//
-//     var themeToggle = function() {
-//       $('body').fadeOut(30, function() {
-//         $('body').removeClass('light-theme');
-//       });
-//       $('body').fadeIn(0, function() {
-//         $('body').addClass('dark-theme');
-//       });
-//       $('#light').addClass('hide');
-//       $('#dark').removeClass('hide');
-//     }
-//
-//     setTimeout(function() {
-//       themeToggle();
-//     }, 250);
-//
-//   },
-//
-//   'click #dark': function() {
-//
-//     var themeToggle = function() {
-//       $('body').fadeOut(30, function() {
-//         $('body').removeClass('dark-theme');
-//       });
-//       $('body').fadeIn(0, function() {
-//         $('body').addClass('light-theme');
-//       });
-//       $('#dark').addClass('hide');
-//       $('#light').removeClass('hide');
-//     }
-//
-//
-//     setTimeout(function() {
-//       themeToggle();
-//     }, 250);
-//
-//   }
-// });
+Template.lightswitch.events({
+
+  'click .themeToggle#light': function() {
+    console.log('hello');
+
+    var themeToggle = function() {
+      $('body').fadeOut(30, function() {
+        $('body').removeClass('light-theme');
+      });
+      $('body').fadeIn(0, function() {
+        $('body').addClass('dark-theme');
+      });
+      $('#light').addClass('hide');
+      $('#dark').removeClass('hide');
+    }
+
+    setTimeout(function() {
+      themeToggle();
+    }, 250);
+
+  },
+
+  'click #dark': function() {
+
+    var themeToggle = function() {
+      $('body').fadeOut(30, function() {
+        $('body').removeClass('dark-theme');
+      });
+      $('body').fadeIn(0, function() {
+        $('body').addClass('light-theme');
+      });
+      $('#dark').addClass('hide');
+      $('#light').removeClass('hide');
+    }
+
+
+    setTimeout(function() {
+      themeToggle();
+    }, 250);
+
+  }
+});
